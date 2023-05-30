b57076e1a0d1e9aa694ba18929f07d5cffaa55ab
kaVII-x <ka52jiguli@yandex.ru>
2023-05-30 22:52:17 +0400

    version 1.1

---
+++ src/test/java/sky/pro/alg/parth2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.alg.parth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
